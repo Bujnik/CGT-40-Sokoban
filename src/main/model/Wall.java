25b274e13a58baa6e2ce2e804e7a8e5130c69b69
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-03-19 19:10:16 +0100

    Part 7
    Let's make the Wall class. A wall can collide with other objects, but it can't move.
    7.1. Add a Wall class to the model package.
    7.2. Make the class inherit the appropriate parent class.
    7.3. Implement a constructor with int x and int y parameters.
    7.4. Implement the method for drawing.
    
    Hint: you could draw walls as brown squares, but no one is limiting your choice of color. Show your full creative potential.

---
+++ src/main/model/Wall.java
@@ -0,0 +1,17 @@
+package main.model;
+
+import java.awt.*;
+
+public class Wall extends CollisionObject{
+    public Wall(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        graphics.setColor(Color.getColor("brown"));
+        graphics.drawRect(x - width / 2, y - height / 2, width, height);
+    }
+
+
+}
