be576216c015e743e6d4ff1411fef637f5029bab
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-03-19 20:20:10 +0100

    Part 11
    Let's begin fleshing out the Model class' functionality. Add the following:
    11.1. A GameObjects gameObjects field. It will store our game objects.
    11.2. An int currentLevel field that stores the current level. Initialize it to 1.
    11.3. A LevelLoader levelLoader field that stores the level loader. Initialize it using the levels.txt file from the res folder.
    11.4. A GameObjects getGameObjects() method that must return all of the game objects.
    11.5. A restartLevel(int level) method that must receive the new game objects from levelLoader for the specified level and save them in the gameObjects field.
    11.6. The restart() method must restart the current level by calling restartLevel with the appropriate argument.
    11.7. The startNextLevel() method must increase the value of the currentLevel variable, which stores the current level number, and restart the new level.

--- src/main/model/Model.java
+++ src/main/model/Model.java
@@ -2,11 +2,35 @@ package main.model;
 
 import main.controller.EventListener;
 
+import java.nio.file.Paths;
+
 public class Model {
     public static final int BOARD_CELL_SIZE = 20;
     private EventListener eventListener;
 
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+    private LevelLoader levelLoader = new LevelLoader(Paths.get("F:\\nauka\\CodeGymTasks\\4.JavaCollections\\src\\com\\codegym\\task\\task34\\task3410\\res\\levels.txt"));
+
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public GameObjects getGameObjects(){
+        return gameObjects;
+    }
+
+    public void restartLevel(int level){
+        currentLevel = level;
+        gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart(){
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel(){
+        currentLevel++;
+        restartLevel(currentLevel);
     }
 }
